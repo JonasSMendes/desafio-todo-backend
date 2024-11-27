644b7f3aafc01734eb9d9a70b9cbdd562e2cc53c
jonas mendes <jonas.jsm2903@gmail.com>
2024-11-27 15:24:28 -0300

    criando pacotes

---
+++ src/main/java/br/com/jonasSm/desafio_ToDo/DesafioToDoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonasSm.desafio_ToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioToDoApplication.class, args);
+	}
+
+}
