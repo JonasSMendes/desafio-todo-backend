6bf12be6e9134a1bbe519815717169a9056cc3fd
jonas mendes <jonas.jsm2903@gmail.com>
2024-11-29 15:47:34 -0300

    adicionando banco H2 para testes, criando teste de success

--- src/test/java/br/com/jonasSm/desafio_ToDo/DesafioToDoApplicationTests.java
+++ src/test/java/br/com/jonasSm/desafio_ToDo/DesafioToDoApplicationTests.java
@@ -1,13 +1,41 @@
 package br.com.jonasSm.desafio_ToDo;
 
+import br.com.jonasSm.desafio_ToDo.entity.Todo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioToDoApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
+
+
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("Todo 2 Up", "Todo 2 Up", false, 1);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$[0].length()").isEqualTo(5)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.getRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+
+	@Test
+	void testCreateTodoFailure() {
 	}
 
 }
